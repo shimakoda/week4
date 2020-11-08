ee499d35015dd1b42ed8f2957c688c7965db2b33
shimakoda <54798204+shimakoda@users.noreply.github.com>
2020-11-08 23:24:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const CORS = {
   'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
 };
 require('http').Server((req,res) => {
-if(res.url === '/result4') {
+if(res.url === '/result4/') {
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...CORS});
   //var str = `{ 'message': ${req.body}, 'x-result': 42 }`;
   //res.write(str);

c3da811e47ff261b1858c218797b80728ed3bb31
shimakoda <54798204+shimakoda@users.noreply.github.com>
2020-11-08 23:22:12 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
+};
+require('http').Server((req,res) => {
+if(res.url === '/result4') {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...CORS});
+  //var str = `{ 'message': ${req.body}, 'x-result': 42 }`;
+  //res.write(str);
+  }
+  res.end('OK\n');
+})
+.listen();

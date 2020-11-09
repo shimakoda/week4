e3782309222c81bef59d73309f2a45ce15eb982c
shimakoda <54798204+shimakoda@users.noreply.github.com>
2020-11-09 07:51:53 +0000

    Update index.js

--- public_html/index.js
+++ public_html/index.js
@@ -1,1 +1,15 @@
-shimakoda
+//shimakoda
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
+};
+require('http').Server((req,res) => {
+if(res.url === '/result4') {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8', ...CORS});
+  //var str = `{ 'message': ${req.body}, 'x-result': 42 }`;
+  res.write('shimakoda');
+  }
+  res.end();
+})
+.listen(443);
